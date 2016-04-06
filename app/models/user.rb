bf4f094460093be73b82765a8b5ae75934933fa1
Kyle Skinner <kyleskin@gmail.com>
2016-04-06 10:39:22 -0600

    fixing merge conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,4 @@
-class NameValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A[a-zA-Z]+\z/i
-      record.errors[attribute] << (options[:message] || 'must contain only characters')
-    end
-  end
-end
-
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || 'is not an email')
-    end
-  end
-end
-
 class User < ActiveRecord::Base
   has_secure_password
-  validates :first_name, :last_name, name: true
-  validates :email, email: true, uniqueness: true
+  has_many
 end
