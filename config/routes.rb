1771964ca882c5a1d1fa9a83f77a933fb955e12b
Kyle Skinner <kyleskin@gmail.com>
2016-04-06 13:15:03 -0600

    cleaning up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   resources :items
-  root 'static_pages#index'
-  get '/about' => 'static_pages#about'
 
-  if current_user do
-      root 'items#index', as: 'user'
-    end
-  end
+  get '/about' => 'static_pages#about'
 
   get 'login' => 'sessions#login'
   post 'login' => 'sessions#create'
@@ -16,5 +11,7 @@ Rails.application.routes.draw do
   resources :users,
             only: [:new, :create],
             path_names: {new: "create account"}
+
+  root 'static_pages#index'
 
 end
