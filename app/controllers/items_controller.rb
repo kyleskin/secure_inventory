83c3d2c9e9a605df200d55a0727e8fdac4dbeae6
Kyle Skinner <kyleskin@gmail.com>
2016-04-06 11:07:25 -0600

    Only user’s items are displayed on index page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.where(user_id: current_user)
   end
 
   # GET /items/1
